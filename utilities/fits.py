dbdfbcb665f2b3eabb9864c236c62aa252127ebc
Rohit Khoiwal <golukhoiwal16@gmail.com>
2022-06-21 15:58:27 +0530

    updated fit function

--- utilities/fits.py
+++ utilities/fits.py
@@ -1,19 +1,31 @@
 import jax
 import jax.numpy as jnp
 import optax
+from functools import partial
 
-def fit(model, params, X, y, learning_rate = 0.01, epochs=1000, verbose=False):
-    
+def fit(model, params, auxs, learning_rate = 0.01, epochs=1000, random_seed = 0, verbose=False):
+
+    rng = jax.random.PRNGKey(random_seed)
+
     opt = optax.adam(learning_rate=learning_rate)
     opt_state = opt.init(params)
 
-    loss_grad_fn = jax.jit(jax.value_and_grad(model.loss_fn))
+    loss_fn = partial(model.loss_fn, **auxs)
+    loss_grad_fn = jax.value_and_grad(loss_fn)
     losses = []
-    for i in range(epochs):
-        loss_val, grads = loss_grad_fn(params, X, y)
+
+    @jax.jit
+    def one_epoch(params, opt_state, rng):
+        loss_val, grads = loss_grad_fn(params, rng=rng)
         updates, opt_state = opt.update(grads, opt_state, params)
         params = optax.apply_updates(params, updates)
+        return params, opt_state, loss_val
+
+    for i in range(epochs):
+        rng, _ = jax.random.split(rng)
+        params, opt_state, loss_val = one_epoch(params, opt_state, rng)
         losses.append(loss_val)
         if verbose and i % (epochs / 10) == 0:
             print('Loss step {}: '.format(i), loss_val)
+
     return params, jnp.array(losses)
