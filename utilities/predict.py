9d2e37d29f056d1ec7ef65e494434697b12bb495
VibhutiBansal-11 <bansal.vibhuti25@gmail.com>
2022-06-21 16:23:11 +0530

    changes de

--- utilities/predict.py
+++ utilities/predict.py
@@ -3,7 +3,7 @@ def predict(n_models,model,params_list,X):
     means_list = []
     sigmas_list = []
     for i in range(n_models):
-        mean,sigma = model.apply(params_list[i], X)
+        mean,sigma = model.apply(params_list[i], X,deterministic=True)
         means_list.append(mean)
         sigmas_list.append(sigma)
     means = jnp.stack(means_list)
