3f20abf0caf9a409641227a77571a11575786caf
Rohit Khoiwal <golukhoiwal16@gmail.com>
2022-06-21 18:16:45 +0530

    updated fit function

--- utilities/fits.py
+++ utilities/fits.py
@@ -21,11 +21,15 @@ def fit(model, params, auxs, learning_rate = 0.01, epochs=1000, random_seed = 0,
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss_val
 
-    for i in range(epochs):
-        rng, _ = jax.random.split(rng)
-        params, opt_state, loss_val = one_epoch(params, opt_state, rng)
-        losses.append(loss_val)
-        if verbose and i % (epochs / 10) == 0:
-            print('Loss step {}: '.format(i), loss_val)
+    try:
+        for i in range(epochs):
+            rng, _ = jax.random.split(rng)
+            params, opt_state, loss_val = one_epoch(params, opt_state, rng)
+            losses.append(loss_val)
+            if verbose and i % (epochs / 10) == 0:
+                print('Loss step {}: '.format(i), loss_val)
+    except KeyboardInterrupt:
+        print(f"Completed {i} epochs.")
+        return params, jnp.array(losses)    
 
     return params, jnp.array(losses)
