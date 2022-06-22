afd3ecc8f70637b090dd7959aa228f58700fc59f
VibhutiBansal-11 <bansal.vibhuti25@gmail.com>
2022-06-23 01:04:07 +0530

    ysort bug fix

--- utilities/plot.py
+++ utilities/plot.py
@@ -45,22 +45,23 @@ def calibration_regression(mean,sigma,Y,color,label,ax=None):
     ax.plot(k,counts,color=color,label=label)
     
     ax.plot(k,counts,'o',color=color)
-    ax.plot(k,k,'o',color='red')
+    ax.plot(k,k,'o',color='green')
     ax.set_yticks(k)
     ax.set_xticks(k)
     ax.legend()
-    ax.plot(k,k,color='red',label='Ideal')
+    ax.plot(k,k,color='green',label='Ideal')
     sns.despine()
     return df,df2
 def plot_prediction(X,Y,x_stack,y_stack,mean,sigma,title,ax=None,n_points=300):
     if ax==None:
       fig,ax=plt.subplots(1)
+    # x_stack
     ax.plot(x_stack,mean, color='red',linewidth=3)
     for i_std in range(1,4):
-      ax.fill_between(x_stack.reshape(n_points), jnp.array((mean-i_std*sigma)), jnp.array((mean+i_std*sigma)), color='orange',alpha=2/(3*i_std), label=f'$\mu\pm{i_std}\sigma$')
-    ax.scatter(x_stack[:int(n_points/3)], y_stack[:int(n_points/3)],color='blue',alpha=0.5)
+      ax.fill_between(x_stack.reshape(n_points), jnp.array((mean-i_std*sigma)), jnp.array((mean+i_std*sigma)), color='lightsalmon',alpha=2/(3*i_std), label=f'$\mu\pm{i_std}\sigma$')
+    ax.scatter(x_stack[:int(n_points/3)], y_stack[:int(n_points/3)],color='crimson',alpha=0.5)
     ax.scatter(X, Y,color='black',alpha=0.7)
-    ax.scatter(x_stack[int(n_points*2/3):], y_stack[int(n_points*2/3):],color='blue',alpha=0.5)
+    ax.scatter(x_stack[int(n_points*2/3):], y_stack[int(n_points*2/3):],color='crimson',alpha=0.5)
     ax.vlines(min(X),min(min(y_stack),min(mean-3*sigma)),max(max(y_stack),max(mean+3*sigma)),colors='black',linestyles='--')
     ax.vlines(max(X),min(min(y_stack),min(mean-3*sigma)),max(max(y_stack),max(mean+3*sigma)),colors='black',linestyles='--')
     ax.set_xlabel("$x$")
