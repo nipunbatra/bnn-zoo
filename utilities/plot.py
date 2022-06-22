988456775e0e178282bf7eabadb94a24482eb1a9
VibhutiBansal-11 <bansal.vibhuti25@gmail.com>
2022-06-22 20:20:26 +0530

    hetero

--- utilities/plot.py
+++ utilities/plot.py
@@ -15,10 +15,10 @@ def plot_actualdata(X,Y,x_test_1,y_test_1,x_test_2,y_test_2):
 def calibration_reg(mean,sigma,Y,ax=None):
     mean_train = mean[100:200]
     sigma_train = sigma[100:200]
-    mean_test = mean[:100]+mean[200:]
-    sigma_test  =sigma[:100]+sigma[200:]
     y_train = Y[100:200]
-    y_test=Y[:100]+Y[200:]
+    mean_test = jnp.concatenate([mean[:100],mean[200:]])
+    sigma_test  =jnp.concatenate([sigma[:100],sigma[200:]])
+    y_test=jnp.concatenate([Y[:100],Y[200:]])
     _,_ = calibration_regression(mean_train,sigma_train,y_train,'black','Train',ax)
     _,_ = calibration_regression(mean_test,sigma_test,y_test,'crimson','Test',ax)
 def calibration_regression(mean,sigma,Y,color,label,ax=None):
@@ -57,10 +57,10 @@ def plot_prediction(X,Y,x_stack,y_stack,mean,sigma,title,ax=None,n_points=300):
       fig,ax=plt.subplots(1)
     ax.plot(x_stack,mean, color='red',linewidth=3)
     for i_std in range(1,4):
-      ax.fill_between(x_stack.reshape(n_points), jnp.array((mean-i_std*sigma)), jnp.array((mean+i_std*sigma)), color='lightsalmon',alpha=2/(3*i_std), label=f'$\mu\pm{i_std}\sigma$')
-    ax.scatter(x_stack[:int(n_points/3)], y_stack[:int(n_points/3)],color='crimson',alpha=0.5)
+      ax.fill_between(x_stack.reshape(n_points), jnp.array((mean-i_std*sigma)), jnp.array((mean+i_std*sigma)), color='orange',alpha=2/(3*i_std), label=f'$\mu\pm{i_std}\sigma$')
+    ax.scatter(x_stack[:int(n_points/3)], y_stack[:int(n_points/3)],color='blue',alpha=0.5)
     ax.scatter(X, Y,color='black',alpha=0.7)
-    ax.scatter(x_stack[int(n_points*2/3):], y_stack[int(n_points*2/3):],color='crimson',alpha=0.5)
+    ax.scatter(x_stack[int(n_points*2/3):], y_stack[int(n_points*2/3):],color='blue',alpha=0.5)
     ax.vlines(min(X),min(min(y_stack),min(mean-3*sigma)),max(max(y_stack),max(mean+3*sigma)),colors='black',linestyles='--')
     ax.vlines(max(X),min(min(y_stack),min(mean-3*sigma)),max(max(y_stack),max(mean+3*sigma)),colors='black',linestyles='--')
     ax.set_xlabel("$x$")
