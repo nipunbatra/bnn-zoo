2a52170e3f28cbe473f7afd4c989a794a9dd9483
Rohit Khoiwal <golukhoiwal16@gmail.com>
2022-06-28 19:14:00 +0530

    Add batching in fit method.

--- utilities/fits.py
+++ utilities/fits.py
@@ -3,33 +3,31 @@ import jax.numpy as jnp
 import optax
 from functools import partial
 
-def fit(model, params, auxs, learning_rate = 0.01, epochs=1000, random_seed = 0, verbose=False):
-
-    rng = jax.random.PRNGKey(random_seed)
-
+def fit(model, params, X, y, deterministic, batch_size = 32, learning_rate=0.01, epochs=10, rng=jax.random.PRNGKey(0)):
     opt = optax.adam(learning_rate=learning_rate)
     opt_state = opt.init(params)
-
-    loss_fn = partial(model.loss_fn, **auxs)
+    
+    loss_fn = partial(model.loss_fn, deterministic=deterministic)
     loss_grad_fn = jax.value_and_grad(loss_fn)
     losses = []
-
+    total_epochs = (len(X)//batch_size) * epochs
+    
+    carry = {}
+    carry["params"] = params
+    carry["state"] = opt_state
+    
     @jax.jit
-    def one_epoch(params, opt_state, rng):
-        loss_val, grads = loss_grad_fn(params, rng=rng)
+    def one_epoch(carry, rng):
+        params = carry["params"]
+        opt_state = carry["state"]
+        idx = jax.random.choice(rng, jnp.arange(len(X)), shape=(batch_size,), replace=False)
+        loss_val, grads = loss_grad_fn(params, X[idx], y[idx], rng=rng)
         updates, opt_state = opt.update(grads, opt_state, params)
         params = optax.apply_updates(params, updates)
-        return params, opt_state, loss_val
-
-    try:
-        for i in range(epochs):
-            rng, _ = jax.random.split(rng)
-            params, opt_state, loss_val = one_epoch(params, opt_state, rng)
-            losses.append(loss_val)
-            if verbose and i % (epochs / 10) == 0:
-                print('Loss step {}: '.format(i), loss_val)
-    except KeyboardInterrupt:
-        print(f"Completed {i} epochs.")
-        return params, jnp.array(losses)    
-
-    return params, jnp.array(losses)
+        carry["params"] = params
+        carry["state"] = opt_state
+        
+        return carry, loss_val
+    
+    carry, losses = jax.lax.scan(one_epoch, carry, jax.random.split(rng, total_epochs))
+    return carry["params"], losses
