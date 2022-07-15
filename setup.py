870a364e2c315818b9be858609ca81869b049a28
Haikoo Khandor <85190504+haikookhandor@users.noreply.github.com>
2022-07-15 15:34:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ with open("requirements.txt") as f:
 setup(
     name="bnn_zoo",
     version="0.1",
+    description = "MLP(Multi-layer Perceptron) from scratch using JAX"
     url="https://github.com/nipunbatra/bnn-zoo/tree/pip-install-try1"
-    python_requires=">=3.6",
     install_requires=requirements,
     packages=setuptools.find_packages(),
-    include_package_data=True,
 )
